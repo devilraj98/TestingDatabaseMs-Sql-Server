be4210479b2bbf5a252d1dda5faf36d58d9c6d4e
[Neeraj kumar] <[neerajraj985275@gmail.com]>
2021-08-16 02:44:31 +0530

    first commit

---
+++ app/src/main/java/com/neeraj/mysqlservertesting/MainActivity.java
@@ -0,0 +1,72 @@
+package com.neeraj.mysqlservertesting;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    private static String ip = "http://database2.crvszuq21sn8.us-east-2.rds.amazonaws.com/";
+    private static String port = "1433";
+    private static String Classes = "net.sourceforge.jtds.jdbc.Driver";
+    private static String database = "testDatabase";
+    private static String username = "redcandatanode";
+    private static String password = "redcanpass";
+    private static String url = "jdbc:jtds:sqlserver://"+ip+":"+port+"/"+database;
+
+    private Connection connection = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
+        textView = findViewById(R.id.textView);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            Class.forName(Classes);
+            connection = DriverManager.getConnection(url, username,password);
+            textView.setText("SUCCESS");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            textView.setText("ERROR");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            textView.setText("FAILURE");
+        }
+    }
+
+    public void sqlButton(View view){
+        if (connection!=null){
+            Statement statement = null;
+            try {
+                statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("Select * from TEST_TABLE;");
+                while (resultSet.next()){
+                    textView.setText(resultSet.getString(1));
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            textView.setText("Connection is null");
+        }
+    }
+}
